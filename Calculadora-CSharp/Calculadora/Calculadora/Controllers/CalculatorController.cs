34991cd30868e246af10ce82ccab4aadbf3e28a6
Gabriel Silva Torres <gabrielst328@gmail.com>
2024-03-04 22:13:14 -0300

    Operações multiplicação, divisão, média e raiz quadrada implementadas

--- Calculadora-CSharp/Calculadora/Calculadora/Controllers/CalculatorController.cs
+++ Calculadora-CSharp/Calculadora/Calculadora/Controllers/CalculatorController.cs
@@ -36,6 +36,51 @@ namespace Calculadora.Controllers
             return BadRequest("Invalid input");
         }
 
+        [HttpGet("mult/{firstNumber}/{secondNumber}")]
+        public IActionResult GetMult(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sub = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(sub.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        public IActionResult GetDiv(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sub = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(sub.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("mid/{firstNumber}/{secondNumber}")]
+        public IActionResult GetMid(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sub = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+                return Ok(sub.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("sqrt/{number}")]
+        public IActionResult GetSqrt(string number)
+        {
+            if (IsNumeric(number))
+            {
+                var num = ConvertToDouble(number);
+                var sub = Math.Sqrt(num);
+                return Ok(sub.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+
         private bool IsNumeric(string strNumber)
         {
             double number;
@@ -44,6 +89,16 @@ namespace Calculadora.Controllers
                 System.Globalization.NumberFormatInfo.InvariantInfo,
                 out number);
             return isNumber;
+        }
+
+        private double ConvertToDouble(string strNumber)
+        {
+            double doubleValue;
+            if(double.TryParse(strNumber, out doubleValue))
+            {
+                return doubleValue;
+            }
+            return doubleValue;
         }
 
         private decimal ConvertToDecimal(string strNumber)
--- Calculadora-CSharp/Calculadora/Calculadora/obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
+++ Calculadora-CSharp/Calculadora/Calculadora/obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bc1b91061409d16586ed3d4fd286cf10d516a80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c91614dbe3059de56a70c769a1f651a9a8f2ad6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
